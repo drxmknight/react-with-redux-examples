b603a6d01bd2b0528cd05a5a2abe1ab5c1cc0293
Eduardo Hales <negro.hales@gmail.com>
2019-01-21 21:47:48 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// Import exercices
+import './ex1-intro/index';
+import './ex2-components/index';
+import './ex3-seasons/index';
+import './ex4-pics/index';
